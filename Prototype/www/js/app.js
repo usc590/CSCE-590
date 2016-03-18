475e07f601dbc2c8ab6d8a1aab1c2b713b17b7a7
shawn <guanx3@yahoo.com>
2016-03-18 08:26:51 -0400

    version3

---
+++ Prototype/www/js/app.js
@@ -0,0 +1,109 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.menus', {
+      url: '/menus',
+      views: {
+        'tab-menus': {
+          templateUrl: 'templates/tab-menus.html',
+          controller: 'MenusCtrl'
+        }
+      }
+    })
+    .state('tab.menu-detail', {
+      url: '/menus/:menuId',
+      views: {
+        'tab-menus': {
+          templateUrl: 'templates/menu-detail.html',
+          controller: 'MenuDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  })
+
+      .state('tab.location', {
+      url: '/location',
+      views: {
+        'tab-location': {
+          templateUrl: 'templates/tab-location.html',
+          controller: 'LocationCtrl'
+        }
+      }
+    })
+
+          .state('tab.order', {
+      url: '/tab-order',
+      views: {
+        'tab-menus': {
+          templateUrl: 'templates/tab-order.html',
+          controller: 'OrderCtrl'
+        }
+      }
+    })
+
+          
+
+;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/account');
+
+});
---
+++ Prototype/www/js/controllers.js
@@ -0,0 +1,65 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('LocationCtrl', function($scope) {})
+
+.controller('OrderCtrl', function($scope) {})
+
+.controller('MenusCtrl', function($scope, Menus) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.menus = Menus.all();
+  $scope.remove = function(menu) {
+    Menus.remove(menu);
+  };
+})
+
+.controller('MenuDetailCtrl', function($scope, $stateParams, Menus) {
+  $scope.menu = Menus.get($stateParams.menuId);
+})
+
+.controller('AccountCtrl', function($scope, $state) {
+
+var APPLICATION_ID = '39A2CA58-20B2-F790-FFF0-A4239F494300',
+    SECRET_KEY = 'C880CB24-6D10-8E4C-FF7E-9C94567FC500',
+    VERSION = 'v1'; //default application version;
+
+Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+
+/*var self = this;
+self.email = "";
+self.password = "";
+self.name = "";*/
+/*$scope.register = function () {
+  console.log("save to new page");
+  console.log(self.email+" "+self.name);
+  function userRegister
+}*/
+
+/*var user = new Backendless.User();
+user.email = $scope.email;
+user.password = $scope.password;
+//user.name=self.name;
+
+Backendless.UserService.register(user, new Backendless.Async(this.userRegistered, this.gotError));
+//$state.go("tab.menus");*/
+
+$scope.register=function() {
+  
+  console.log($scope.controller.email);
+}
+/*var user = new Backendless.User();
+user.email = $scope.form.email;
+user.password = $scope.form.password;
+
+Backendless.UserService.register(user);*/
+});
+
+
